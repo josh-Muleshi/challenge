fd93b0d7b041b24e3db0076361b9b89de5389418
Josh-Muleshi <jmuleshi2@gmail.com>
2022-05-31 00:08:23 +0200

    last commit

--- app/src/main/java/cd/wayupdev/challenger/MainActivity.kt
+++ app/src/main/java/cd/wayupdev/challenger/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import cd.wayupdev.challenger.ui.screen.SplashScreen
 import cd.wayupdev.challenger.ui.theme.ChallengerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ChallengerTheme {
                 //val navController = rememberNavController()
